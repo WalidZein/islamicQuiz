13313bcf0d80ff63cd472fe5f47c9a6fd22bc0ea
wmoz <wmoz@vt.edu>
2024-11-18 23:25:28 -0600

    update leaderboard page metadata

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,5 +1,13 @@
 import Leaderboard from '../components/Leaderboard';
 
+// Generate metadata for SEO
+export function generateMetadata() {
+
+    return {
+        title: 'Islamic Quiz: Leaderboard',
+    };
+}
+
 export default function LeaderboardPage() {
     return (
         <div className="container mx-auto">
