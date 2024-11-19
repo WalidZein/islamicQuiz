dae4bc7cc072adb1eb547b5df73dd2c9b66866b0
wmoz <wmoz@vt.edu>
2024-11-18 23:20:16 -0600

    quiz 17 release time update

--- src/data/quizzes.ts
+++ src/data/quizzes.ts
@@ -745,7 +745,7 @@ const quizzes: Quiz[] = [
   {
     id: 17,
     released: true,
-    timeOfRelease: "2024-11-17T13:00:00Z",
+    timeOfRelease: "2024-11-18T13:00:00Z",
     questions: [
       {
         question: "What did the Prophet Muhammad ﷺ say about removing something harmful from the road?",
